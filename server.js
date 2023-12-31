9589804a3ee76001faa78965b986d7924c7ee0a1
Nick Friesen <nfriesen26@gmail.com>
2023-12-30 21:04:27 -0800

    Updated syntax. Still getting error.

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 import * as dotenv from 'dotenv';
 dotenv.config();
 
-import { Configuration, OpenAIApi } from 'openai';
+const { Configuration, OpenAIApi } = pkg;
+import pkg from 'openai-api';
 
 const configuration = new Configuration({ apiKey: process.env.OPENAI });
 const openai = new OpenAIApi(configuration);
