21c56995fc864c0d1ed0bc8d63ef6f5798af3d5c
Nick Friesen <nfriesen26@gmail.com>
2024-01-01 14:10:07 -0800

    Update page title and remove unused code

--- main.js
+++ main.js
@@ -1,28 +1,3 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
-
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
-
-setupCounter(document.querySelector('#counter'))
-/*
 import './style.css';
 
 const form = document.querySelector('form');
@@ -47,4 +22,3 @@ form.addEventListener('submit', async (e) => {
     const result = document.querySelector('#result');
     result.innerHTML = `<img src="${image}" width="512" />`;
 });
-*/
